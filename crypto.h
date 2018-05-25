65fb332b4c2e15fcf1cfb7ed4cf0912e5cbc51c2
a.jafari <alijafari2019@gmail.com>
2018-05-25 17:18:43 +0430

    init

---
+++ crypto.h
@@ -0,0 +1,16 @@
+//
+// Created by Ali on 5/25/2018 AD.
+//
+
+#ifndef CLION_PROJECTS_CRYPTO_H
+#define CLION_PROJECTS_CRYPTO_H
+
+
+class crypto {
+public:
+    crypto();
+    bool* toBinary(int value);
+};
+
+
+#endif //CLION_PROJECTS_CRYPTO_H
